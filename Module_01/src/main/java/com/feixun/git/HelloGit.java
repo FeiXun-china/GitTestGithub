a0c1c3d33fbb8f08b965b25942445cae5a88a39b
fxccie <fxccie_strive@aliyun.com>
2021-01-20 14:55:28 +0800

    第一次提交

---
+++ Module_01/src/main/java/com/feixun/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.feixun.git;
+
+/**
+ * @author feixun
+ * @create 2021-01-20 14:42
+ */
+public class HelloGit {
+
+    public static void main(String[] args){
+        System.out.println("Hello,git!");
+    }
+}
